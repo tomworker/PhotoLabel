969bd6e1de619b30b12d7333402adb979e0697ed
tomworker <tom106worker>
2023-06-11 21:10:46 +0900

    Initial Commit

---
+++ PhotoLabel/PhotoLabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLabelApp.swift
+//  PhotoLabel
+//
+//  Created by tomworker on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
